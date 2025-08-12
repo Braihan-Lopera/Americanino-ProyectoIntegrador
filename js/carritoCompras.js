145c8da15d7c88720347bf236434071d0c072a87
Braihan-Lopera <braihanlopera@gmail.com>
2025-08-12 10:06:30 -0500

    arreglo carritoCompras

--- js/carritoCompras.js
+++ js/carritoCompras.js
@@ -1,55 +1,22 @@
-export async function  mostrarCarrito() {
+export function mostrarCarrito() {
     const btnCarrito = document.getElementById('btnCarrito');
-    const response = await fetch("../data/muestraProductos.json");
-    const data = await response.json();
 
     btnCarrito.addEventListener('click', () => {
-
-        
-
         let divCarrito = document.querySelector('.divCarrito');
 
-       
+        // Si no existe, lo creamos con el diseño final
         if (!divCarrito) {
             divCarrito = document.createElement('div');
             divCarrito.className = 'divCarrito';
             divCarrito.id = 'cartDrawer';
 
-
-            if (JSON.parse(localStorage.getItem("productos")).length == 0){
-                divCarrito.innerHTML = `
+            divCarrito.innerHTML = `
                 <button class="btn-cerrar" id="cerrarCarrito">×</button>
                 <h2>Tu carrito de compras</h2>
                 <div class="icono-carrito">🛒</div>
                 <p>Tu carrito está vacía</p>
                 <a href="#">Ver todo</a>
             `;
-            }else{
-                let productosGuardados = JSON.parse(localStorage.getItem("productos")) || []
-                let divtexto = document.createElement("div")
-                divtexto.innerHTML =  `<button class="btn-cerrar" id="cerrarCarrito">×</button>
-                <h2>Tu carrito de compras</h2>`
-                divCarrito.appendChild(divtexto)
-                productosGuardados.forEach(productoGuardado => {
-                    
-                    for (let index = 0; index < data.length; index++) {
-                        if (data[index].id == productoGuardado.id) {
-
-                            let divProductoCarrito = document.createElement("div")
-                            divProductoCarrito.className = "divProductoCarrito"
-                            divProductoCarrito.innerHTML = `<div class = "contenedorCarrito1"><img src="../imagenes/fotosProductos/${data[index].fotoPortada}"></div>
-                            <div class = "contenedorCarrito2"><p>${data[index].nombre}</p>
-                            <p>$ ${data[index].precio}</p>
-                            <p>${productoGuardado.talla}</p></div>`
-                            
-                            divCarrito.appendChild(divProductoCarrito)
-                            
-                        }
-                    }
-
-                });
-            }
-            
 
             document.body.appendChild(divCarrito);
 
@@ -63,4 +30,3 @@ export async function  mostrarCarrito() {
         divCarrito.classList.add('mostrar');
     });
 }
-
--- js/componentes/barraNavegacion.js
+++ js/componentes/barraNavegacion.js
@@ -107,7 +107,7 @@ const actualizarUIUsuario = () => {
             <a href="#" id="enlaceLogout" class="enlaceNavegacion"> <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-logout"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" /><path d="M9 12h12l-3 -3" /><path d="M18 15l3 -3" /></svg> </a>
             
             <a href="" id="btnCarrito" class="enlaceNavegacion" aria-label="Carrito de compras">
-                <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-shopping-bag"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6.331 8h11.339a2 2 0 0 1 1.977 2.304l-1.255 8.152a3 3 0 0 1 -2.966 2.544h-6.852a3 3 0 0 1 -2.965 -2.544l-1.255 -8.152a2 2 0 0 1 1.977 -2.304z" /><path d="M9 11v-5a3 3 0 0 1 6 0v5" /></svg>
+                <svg class="iconoNavbarSvg" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>
             </a>
         `;
 
@@ -168,7 +168,7 @@ export const crearBarraNavegacion = (contenedor, idBody) => {
         <nav class="navegacionPrincipal">
             <div class="seccionIzquierda">
                 <button id="botonMenuHamburguesa" class="iconoNavegacion">☰</button>
-                <button id="botonBusqueda" class="iconoNavegacion">🔍</button>
+                <button id="botonBusqueda" class="iconoNavegacion"><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-search"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" /><path d="M21 21l-6 -6" /></svg></button>
             </div>
             <div class="seccionCentro">
                 <a href="/" id="logoPrincipal">
